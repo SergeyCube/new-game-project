32d3d900c13b9aa4361be9474712f3753e605781
SergeyCube <jmymailbox@gmail.com>
2026-02-04 13:35:37 +0300

    Syncing Label3d.text

--- character.gd
+++ character.gd
@@ -1,6 +1,11 @@
 class_name Character
 extends CharacterBody3D
 
+#@onready var gdsync_global_basis := self.global_basis
+
 func _ready() -> void:
 	$Camera3D.look_at(self.global_transform.origin)
-	print("global basis ", self.global_basis)
+	#print("global basis ", self.global_basis)
+
+#func _physics_process(_delta: float) -> void:
+	#self.gdsync_global_basis = self.global_basis
