99b0728c9e78e4a1fe5fd20af92eb5fe29c7d311
SergeyCube <jmymailbox@gmail.com>
2026-02-06 18:35:54 +0300

    Initial ENet

--- character.gd
+++ character.gd
@@ -1,11 +1,18 @@
 class_name Character
 extends CharacterBody3D
 
-#@onready var gdsync_global_basis := self.global_basis
+func _enter_tree() -> void:
+	self.set_multiplayer_authority(self.name.to_int())
+	if is_multiplayer_authority():
+		self.get_node("Camera3D").current = true
+	else:
+		self.get_node("Walking").remove_from_group("walking")
+		self.get_node("Gravity").remove_from_group("gravity")
+		self.get_node("MoveAndSlide").remove_from_group("move_and_slide")
+		self.get_node("Camera3D").current = false
 
 func _ready() -> void:
 	$Camera3D.look_at(self.global_transform.origin)
-	#print("global basis ", self.global_basis)
-
-#func _physics_process(_delta: float) -> void:
-	#self.gdsync_global_basis = self.global_basis
+	self.global_position = Vector3(0.0, 10.8, 0.0)
+	self.velocity = Vector3(0.0, 0.0, 0.0)
+	
--- game.gd
+++ game.gd
@@ -1,36 +1,12 @@
 extends Node3D
 
-#const Character = preload("uid://d3i1s0eatkbgn") # character.gd
-
-@export var lobby_number: int = 1
-
-var enemy_client_id: int = -1
+const CHARACTER = preload("uid://ckudr8chj1kgo") # character scene
+const PORT: int = 5000
 
 func _ready() -> void:
-	GDSync.connected.connect(_on_GDSync_connected)
-	GDSync.connection_failed.connect(_on_GDSync_connection_failed)
-	
-	GDSync.lobby_created.connect(_on_GDSync_lobby_created)
-	GDSync.lobby_creation_failed.connect(_on_GDSync_lobby_creation_failed)
-	
-	GDSync.lobby_joined.connect(_on_GDSync_lobby_joined)
-	GDSync.lobby_join_failed.connect(_on_GDSync_lobby_join_failed)
-	GDSync.client_id_changed.connect(_on_GDSync_client_id_changed)
-	GDSync.host_changed.connect(_on_GDSync_host_changed)
-	
-	GDSync.client_joined.connect(_on_GDSync_client_joined)
-	GDSync.client_left.connect(_on_GDSync_client_left)
-	
-	#await get_tree().create_timer(10).timeout
-	GDSync.start_multiplayer()
+	pass
 
 func _physics_process(delta: float) -> void:
-	if self.enemy_client_id != -1:
-		var perceived_ping: float = await GDSync.get_client_percieved_ping(self.enemy_client_id)
-		print(GDSync.get_client_id(), " Perceived ping to ", self.enemy_client_id, 
-			": ",  snapped(perceived_ping*1000, 1))
-
-	
 	# Walking with RMB. First, bc initiializes parent's x and z velocity
 	get_tree().call_group("walking", "update_parent_velocity", delta)
 	# Gravity
@@ -47,92 +23,116 @@ normal: Vector3, _shape_idx: int) -> void:
 	for node in get_tree().get_nodes_in_group("walking"):
 		node.destination = Vector3(event_position.x, 0, event_position.z)
 
-#region Multiplayer
+#region ENet_multiplayer
 
-func _on_GDSync_connected() -> void:
-	print(GDSync.get_client_id(), " Connected to GD-Sync")
-	GDSync.player_set_username("Player" + str(GDSync.get_client_id()))
-	#print(GDSync.get_client_id(), " Username ", GDSync.player_get_username(GDSync.get_client_id()))
-	#print(GDSync.get_client_id(), " Player data ", GDSync.player_get_all_data(GDSync.get_client_id()))
-	$ClientIDLabel.text = "ClientID " + str(GDSync.get_client_id())
-	GDSync.lobby_create("Lobby" + str(self.lobby_number), "", true, 2)
-
-func _on_GDSync_connection_failed(error: int) -> void:
-	match(error):
-		ENUMS.CONNECTION_FAILED.INVALID_PUBLIC_KEY:
-			push_error("The public or private key you entered were invalid.")
-		ENUMS.CONNECTION_FAILED.TIMEOUT:
-			push_error("Unable to connect, please check your internet connection.")      
-
-func _on_GDSync_lobby_created(lobby_name: String) -> void:
-	print(GDSync.get_client_id(), " Lobby was created " + lobby_name)
-	GDSync.lobby_join(lobby_name)
-
-func _on_GDSync_lobby_creation_failed(lobby_name: String, error: int) -> void:
-	match(error):
-		ENUMS.LOBBY_CREATION_ERROR.LOBBY_ALREADY_EXISTS:
-			#push_error(str(GDSync.get_client_id()) + " Lobby with the name " + lobby_name + " already exists.")
-			GDSync.lobby_join(lobby_name)
-		ENUMS.LOBBY_CREATION_ERROR.NAME_TOO_SHORT:
-			push_error(lobby_name+" is too short.")
-		ENUMS.LOBBY_CREATION_ERROR.NAME_TOO_LONG:
-			push_error(lobby_name+" is too long.")
-		ENUMS.LOBBY_CREATION_ERROR.PASSWORD_TOO_LONG:
-			push_error("The password for "+lobby_name+" is too long.")
-		ENUMS.LOBBY_CREATION_ERROR.TAGS_TOO_LARGE:
-			push_error("The tags have exceeded the 2048 byte limit.")
-		ENUMS.LOBBY_CREATION_ERROR.DATA_TOO_LARGE:
-			push_error("The data have exceeded the 2048 byte limit.")
-		ENUMS.LOBBY_CREATION_ERROR.ON_COOLDOWN:
-			push_error("Please wait a few seconds before creating another lobby.")
-
-func _on_GDSync_lobby_joined(lobby_name: String) -> void:
-	print(GDSync.get_client_id(), " Joined lobby ", lobby_name)
+func _on_host_button_pressed() -> void:
+	var peer := ENetMultiplayerPeer.new()
+	var error = peer.create_server(PORT, 2)
+	self.multiplayer.multiplayer_peer = peer
+	if error == OK: print(self.multiplayer.get_unique_id(), " Server was created")
+	else: print(error)
+	self.multiplayer.peer_connected.connect(_on_multiplayer_peer_connected)
+	_on_multiplayer_peer_connected(1) # for host itself
 	
-	var character: CharacterBody3D = $CharacterInstantiator.instantiate_node()
-	character.global_position = Vector3(0.0, 10.8, 0.0)
-	character.velocity = Vector3(0.0, 0.0, 0.0)
-	character.name = "Character" + str(GDSync.get_client_id())
-	character.get_node("Label3D").text = str(GDSync.get_client_id())
-	GDSync.set_gdsync_owner(character, GDSync.get_client_id())
-	character.get_node("Camera3D").current = true
+func _on_multiplayer_peer_connected(id: int = 1) -> void:
+	print(id, " connected")
+	var character : Character = CHARACTER.instantiate()
+	character.name = str(id)
+	call_deferred("add_child", character)
 
-func _on_GDSync_lobby_join_failed(lobby_name: String, error: int) -> void:
-	push_error("Failed to join lobby ", lobby_name, " with error ", str(error))
-	
-func _on_GDSync_client_id_changed(own_id: int) -> void:
-	GDSync.player_set_data("ID", own_id)
-	print(GDSync.get_client_id(), " ClientID was changed ")
-	GDSync.player_set_username("Player" + str(own_id))
+func _on_join_button_pressed() -> void:
+	var peer := ENetMultiplayerPeer.new()
+	peer.create_client("127.0.0.1", PORT)
+	self.multiplayer.multiplayer_peer = peer
 
-func _on_GDSync_host_changed(is_host: bool, _new_host_id: int) -> void:
-	if is_host: print(GDSync.get_client_id(), " Host true")
-	else: print(GDSync.get_client_id(), " Host false")
+#endregion
 
-func _on_GDSync_client_joined(client_id: int) -> void:
-	if client_id != GDSync.get_client_id():
-		var ping: float = await GDSync.get_client_ping(client_id)
-		print(GDSync.get_client_id(), " Raw ping to ", client_id, ": ",  snapped(ping*1000, 1))
-		var perceived_ping: float = await GDSync.get_client_percieved_ping(client_id)
-		print(GDSync.get_client_id(), " Perceived ping to ", client_id, ": ",  snapped(perceived_ping*1000, 1))
-		self.enemy_client_id = client_id
+#region GDSync_multiplayer
 
-func _on_GDSync_client_left(client_id: int) -> void:
-	print(client_id, " Left lobby")
-	var character: CharacterBody3D = self.get_node_or_null("Character" + str(client_id))
-	if character != null: character.queue_free()
-
-func _on_character_instantiator_node_instantiated(node: Node) -> void:
-	#print(GDSync.get_client_id(), "    ", self.get_children())
-	if GDSync.is_gdsync_owner(node): return
-	if not node.name.begins_with("Character"): return
-	#if !(node is Character): return
-	#print(GDSync.get_client_id(), "    ", node.name)
-	node.get_node("Walking").remove_from_group("walking")
-	node.get_node("Gravity").remove_from_group("gravity")
-	node.get_node("MoveAndSlide").remove_from_group("move_and_slide")
-	node.get_node("Camera3D").current = false
-	#print(GDSync.get_gdsync_owner(node), " is owner of ", node)
-	#GDSync.sync_var(node.get_node("Label3D"), "text")
+#func _on_GDSync_connected() -> void:
+	#print(GDSync.get_client_id(), " Connected to GD-Sync")
+	#GDSync.player_set_username("Player" + str(GDSync.get_client_id()))
+	##print(GDSync.get_client_id(), " Username ", GDSync.player_get_username(GDSync.get_client_id()))
+	##print(GDSync.get_client_id(), " Player data ", GDSync.player_get_all_data(GDSync.get_client_id()))
+	#$ClientIDLabel.text = "ClientID " + str(GDSync.get_client_id())
+	#GDSync.lobby_create("Lobby" + str(self.lobby_number), "", true, 2)
+#
+#func _on_GDSync_connection_failed(error: int) -> void:
+	#match(error):
+		#ENUMS.CONNECTION_FAILED.INVALID_PUBLIC_KEY:
+			#push_error("The public or private key you entered were invalid.")
+		#ENUMS.CONNECTION_FAILED.TIMEOUT:
+			#push_error("Unable to connect, please check your internet connection.")      
+#
+#func _on_GDSync_lobby_created(lobby_name: String) -> void:
+	#print(GDSync.get_client_id(), " Lobby was created " + lobby_name)
+	#GDSync.lobby_join(lobby_name)
+#
+#func _on_GDSync_lobby_creation_failed(lobby_name: String, error: int) -> void:
+	#match(error):
+		#ENUMS.LOBBY_CREATION_ERROR.LOBBY_ALREADY_EXISTS:
+			##push_error(str(GDSync.get_client_id()) + " Lobby with the name " + lobby_name + " already exists.")
+			#GDSync.lobby_join(lobby_name)
+		#ENUMS.LOBBY_CREATION_ERROR.NAME_TOO_SHORT:
+			#push_error(lobby_name+" is too short.")
+		#ENUMS.LOBBY_CREATION_ERROR.NAME_TOO_LONG:
+			#push_error(lobby_name+" is too long.")
+		#ENUMS.LOBBY_CREATION_ERROR.PASSWORD_TOO_LONG:
+			#push_error("The password for "+lobby_name+" is too long.")
+		#ENUMS.LOBBY_CREATION_ERROR.TAGS_TOO_LARGE:
+			#push_error("The tags have exceeded the 2048 byte limit.")
+		#ENUMS.LOBBY_CREATION_ERROR.DATA_TOO_LARGE:
+			#push_error("The data have exceeded the 2048 byte limit.")
+		#ENUMS.LOBBY_CREATION_ERROR.ON_COOLDOWN:
+			#push_error("Please wait a few seconds before creating another lobby.")
+#
+#func _on_GDSync_lobby_joined(lobby_name: String) -> void:
+	#print(GDSync.get_client_id(), " Joined lobby ", lobby_name)
+	#
+	#var character: CharacterBody3D = $CharacterInstantiator.instantiate_node()
+	#character.global_position = Vector3(0.0, 10.8, 0.0)
+	#character.velocity = Vector3(0.0, 0.0, 0.0)
+	#character.name = "Character" + str(GDSync.get_client_id())
+	#character.get_node("Label3D").text = str(GDSync.get_client_id())
+	#GDSync.set_gdsync_owner(character, GDSync.get_client_id())
+	#character.get_node("Camera3D").current = true
+#
+#func _on_GDSync_lobby_join_failed(lobby_name: String, error: int) -> void:
+	#push_error("Failed to join lobby ", lobby_name, " with error ", str(error))
+	#
+#func _on_GDSync_client_id_changed(own_id: int) -> void:
+	#GDSync.player_set_data("ID", own_id)
+	#print(GDSync.get_client_id(), " ClientID was changed ")
+	#GDSync.player_set_username("Player" + str(own_id))
+#
+#func _on_GDSync_host_changed(is_host: bool, _new_host_id: int) -> void:
+	#if is_host: print(GDSync.get_client_id(), " Host true")
+	#else: print(GDSync.get_client_id(), " Host false")
+#
+#func _on_GDSync_client_joined(client_id: int) -> void:
+	#if client_id != GDSync.get_client_id():
+		#var ping: float = await GDSync.get_client_ping(client_id)
+		#print(GDSync.get_client_id(), " Raw ping to ", client_id, ": ",  snapped(ping*1000, 1))
+		#var perceived_ping: float = await GDSync.get_client_percieved_ping(client_id)
+		#print(GDSync.get_client_id(), " Perceived ping to ", client_id, ": ",  snapped(perceived_ping*1000, 1))
+		#self.enemy_client_id = client_id
+#
+#func _on_GDSync_client_left(client_id: int) -> void:
+	#print(client_id, " Left lobby")
+	#var character: CharacterBody3D = self.get_node_or_null("Character" + str(client_id))
+	#if character != null: character.queue_free()
+#
+#func _on_character_instantiator_node_instantiated(node: Node) -> void:
+	##print(GDSync.get_client_id(), "    ", self.get_children())
+	#if GDSync.is_gdsync_owner(node): return
+	#if not node.name.begins_with("Character"): return
+	##if !(node is Character): return
+	##print(GDSync.get_client_id(), "    ", node.name)
+	#node.get_node("Walking").remove_from_group("walking")
+	#node.get_node("Gravity").remove_from_group("gravity")
+	#node.get_node("MoveAndSlide").remove_from_group("move_and_slide")
+	#node.get_node("Camera3D").current = false
+	##print(GDSync.get_gdsync_owner(node), " is owner of ", node)
+	##GDSync.sync_var(node.get_node("Label3D"), "text")
 
 #endregion
